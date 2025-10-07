559112381676cc9cb8169898f5e69772f16362b6
Martin Yarnold <YOUR_GITHUB_EMAIL@example.com>
2025-10-08 00:42:35 +0100

    chore(frontend): strengthen hls.js shim with default export

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -4,3 +4,7 @@ declare module "rehype-raw";
 declare module "rehype-sanitize";
 declare module "rehype-stringify";
 declare module "hls.js";
+declare module 'hls.js' {
+  const Hls: any;
+  export default Hls;
+}
