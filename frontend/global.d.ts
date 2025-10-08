be23f6557e2b90d80d08ffaad46f3a7addce554f
Martin Yarnold <YOUR_GITHUB_EMAIL@example.com>
2025-10-08 02:33:36 +0100

    ci(typecheck): add ambient module for hls.js (CI fix)

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -4,3 +4,14 @@ declare module 'remark-rehype';
 declare module 'rehype-raw';
 declare module 'rehype-sanitize';
 declare module 'rehype-stringify';
+
+declare module "hls.js" {
+  export default class Hls {
+    static isSupported(): boolean;
+    constructor(config?: any);
+    loadSource(url: string): void;
+    attachMedia(el: HTMLMediaElement): void;
+    on(event: string, handler: (...args: any[]) => void): void;
+    destroy(): void;
+  }
+}
